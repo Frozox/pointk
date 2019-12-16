b0cdbfbe447715d9713304641fd68bb2662e46e6
Swider <andy.frot@outlook.fr>
2019-12-16 14:23:41 +0100

    Bootstrap et MàJ

--- template/interface PointK/index.php
+++ template/interface PointK/index.php
@@ -2,9 +2,45 @@
   <head>
     <title>Point K</title>
     <meta charset="utf-8" />
+    <link rel="stylesheet" href="../../public/css/bootstrap.css" />
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../public/css/bootstrap-grid.css" />
+    <link rel="stylesheet" href="../../public/css/bootstrap-grid.min.css" />
+    <link rel="stylesheet" href="../../public/css/bootstrap-reboot.css" />
+    <link rel="stylesheet" href="../../public/css/bootstrap-reboot.min.css" />
+    <script src="../../public/js/bootstrap.js"></script>
+    <script src="../../public/js/bootstrap.min.js"></script>
+    <script src="../../public/js/bootstrap.bundle.js"></script>
+    <script src="../../public/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
-    <h1>Point K</h1>
-    <nav>Barre de navigation</nav>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bs-dark">
+      <a class="navbar-brand" href="index.php">Gestion Point K</a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Users</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Currencies</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Thresholds</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Add funds logs</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Consumption logs</a>
+          </li>
+        </ul>
+        <button class="btn btn-info" href="#">Help</button>
+        <button class="btn btn-info" href="#">Settings</button>
+        <button class="btn btn-success" href="#">Log out</button>
+      </div>
+    </nav>
   </body>
 </html>
