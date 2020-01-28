e547f019ebb1637fbfe0f17ce425c0fe9c5ed792
Swider <andy.frot@outlook.fr>
2020-01-28 11:56:10 +0100

    JQuery

--- template/interface PointK/index.php
+++ template/interface PointK/index.php
@@ -2,21 +2,12 @@
   <head>
     <title>Point K</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<!--
-    <link rel="stylesheet" href="../../public/css/bootstrap.css" />
     <link rel="stylesheet" href="../../public/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../../public/css/bootstrap-grid.css" />
     <link rel="stylesheet" href="../../public/css/bootstrap-grid.min.css" />
-    <link rel="stylesheet" href="../../public/css/bootstrap-reboot.css" />
     <link rel="stylesheet" href="../../public/css/bootstrap-reboot.min.css" />
-    <script src="../../public/js/bootstrap.js"></script>
+    <script src="../../public/js/jquery-3.4.1.min.js"></script>
     <script src="../../public/js/bootstrap.min.js"></script>
-    <script src="../../public/js/bootstrap.bundle.js"></script>
-    <script src="../../public/js/bootstrap.bundle.min.js"></script>-->
+    <script src="../../public/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bs-dark">
