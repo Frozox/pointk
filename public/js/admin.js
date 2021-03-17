5d8957fc4bcb742ab2e0cfb048bbd89530a67125
Le Guellanff Quentin <quentin.le-guellanff@universite-paris-saclay.fr>
2021-03-17 22:02:57 +0100

    Revert "ADD Finalisation de l'affichage de la liste utilisateur, passage aux produits et commandes"
    This reverts commit 078b2d9c

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-    var produitlist = $('#produit-list');
-    var userlist = $('#user-list');
-    var commandelist = $('#commande-list');
+    var produit_search_timedout = null;
+    var user_search_timedout = null;
+    var commandes_search_timedout = null;
+    var page_produit = 1;
+    var page_user = 1;
+    var page_user_timeout = null;
+    var page_commande = 1;
 
     //AJAX pour formulaire de création de produit
     $(document).on('submit', '#produit-create-form', function (event) {
@@ -93,36 +97,22 @@ $(document).ready(function () {
 
     //AJAX load produit list
     function loadProduitList(){
-        $.ajax({
-            method: "POST",
-            url: findProduitsToList,
-            success: function (data) {
-                if(data['code'] === 200){
-                    produitlist.DataTable().clear();
-                    produitlist.DataTable().destroy();
-                    produitlist.children('tbody').append(data['content']);
-                    produitlist.DataTable({
-                        "autoWidth": true,
-                        "bLengthChange": false,
-                        "bInfo": false
-                    });
-                }
-            }
-        });
+
     }
 
     //AJAX load user list
     function loadUserList(){
+        var userlist = $('#user-list').children('tbody');
+
         $.ajax({
             method: "POST",
             url: findUsersToList,
             success: function (data) {
                 if(data['code'] === 200){
-                    userlist.DataTable().clear();
-                    userlist.DataTable().destroy();
-                    userlist.children('tbody').append(data['content']);
-                    userlist.DataTable({
-                        "autoWidth": true,
+                    userlist.parent().DataTable().clear();
+                    userlist.parent().DataTable().destroy();
+                    userlist.append(data['content']);
+                    userlist.parent().DataTable({
                         "bLengthChange": false,
                         "bInfo": false,
                         columnDefs: [
@@ -136,37 +126,78 @@ $(document).ready(function () {
 
     //AJAX load commande list
     function loadCommandeList(){
-        $.ajax({
-            method: "POST",
-            url: findCommandesToList,
-            success: function (data) {
-                if(data['code'] === 200){
-                    commandelist.DataTable().clear();
-                    commandelist.DataTable().destroy();
-                    commandelist.children('tbody').append(data['content']);
-                    commandelist.DataTable({
-                        "autoWidth": true,
-                        "bLengthChange": false,
-                        "bInfo": false
-                    });
-                }
-            }
-        });
+
     }
+
+    //AJAX pour afficher des produits selon les informations entrées dans la barre de recherche
+    /*$("#produit-list-search").on("keyup", function () {
+
+    });*/
+
+    //AJAX pour afficher des utilisateurs selon les informations entrées dans la barre de recherche
+    /*$("#user-list-search").on("keyup", function () {
+        clearTimeout(user_search_timedout);
+
+        user_search_timedout = setTimeout(() => {
+            var search = $(this).val().toLowerCase();
+            var userlist = $('#user-list').children('tbody');
+            var paginate = $('#paginate-user');
+
+            $.ajax({
+                method: "POST",
+                url: findUsersToList,
+                data: {
+                    search: search,
+                    page: page_user,
+                    sortby: '',
+                    orderby: 'ASC'
+                },
+                success: function (data) {
+                    if (data['code'] === 200) {
+                        userlist.empty();
+                        paginate.empty();
+                        userlist.append(data['content']);
+                        paginate.append(data['paginate']);
+                    }
+                }
+            });
+        }, 750);
+    });*/
+
+    //AJAX pour afficher des produits selon les informations entrées dans la barre de recherche
+    /*$("#commande-list-search").on("keyup", function () {
+
+    });*/
+
+    //Changement de page avec la pagination
+    /*$(document).on('click', '#paginate-user ul li button', function (){
+        clearTimeout(page_user_timeout)
+
+        user_search_timedout = setTimeout(() => {
+            var value = $(this).attr('value')
+            var temp = page_user;
+
+            if (value === 'previous' && page_user > 1) {
+                page_user--;
+            } else if (value === 'after') {
+                page_user++;
+            } else {
+                page_user = value;
+            }
+
+            if (value !== temp) {
+                $('#user-list-search').trigger('keyup');
+            }
+        },750);
+    });*/
 
     loadProduitList();
     loadUserList();
     loadCommandeList();
-
     $(document).one('ajaxStop', function (){
         $('#loading').hide("puff").delay(10).queue(function () {
             $('#loading').remove();
             $('.container-fluid').show();
-            produitlist.DataTable().columns.adjust().draw();
-            userlist.DataTable().columns.adjust().draw();
-            commandelist.DataTable().columns.adjust().draw();
-            $('#user').removeClass("show active");
-            $('#commande').removeClass("show active");
         });
     });
 });
